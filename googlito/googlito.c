4bb5b254c575fd6d8f193f3b4ccb9b639014c488
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-11-19 22:50:08 -0300

    Compĺeted Googlito

--- googlito/googlito.c
+++ googlito/googlito.c
@@ -1,104 +1,332 @@
-//GOOGLITO 2.0
-
 #include<cs50.h>
 #include<stdio.h>
 #include<string.h>
 
-void LeerPalabras(char palabras[20][20]){
-	int c = 0;
-	FILE *archivo;
-	char buffer[20];
-	archivo = fopen("Doc0.txt","r");
-	while(c < 20){
-		fscanf(archivo, "%s", buffer);
-		if(strlen(buffer)>4){
-			strcpy(palabras[c],buffer);
-			c++;
-		}
-	}
+const int MaxDoc=50; // Máximo Núm. de documentos
+const int MaxPal=1000; // Máximo núm. de palabras en Vocabulario
+const int LargoMaxPal=20; // Largo máximo de una palabra
+const int LargoMinPal=3; // Largo mínimo de una palabra
+const int Listado=3; //Número de documentos a mostrar
+int NumPal = 0; // Núm. de palabras actuales del Vocabulario
+int NumeroArchivos = 0;
+int ExistePalabra = 0;
+
+int EncontrarLugar(string consulta,char Vocabulario[MaxPal][LargoMaxPal]){
+    int Lugar = -1;
+    for(int i = 0; i<MaxPal; i++){
+        if(strcmp(consulta,Vocabulario[i])==0){
+            Lugar = i;
+            break;
+        }
+    }
+    return Lugar;
 }
 
-void crear MatrizFreq(char Matriz[50],char vocabulario[20]){
-    char nombre[20];
-    File *archivo;
-    char buffer[20];
-    for(int i=0 ; i<1000 ; i++)
-    {
-    for(int j=0 ; j<50 ; j++)
-    Matriz[i][j]=0;
+void IntArrayPrinter(int arreglo[], int size){
+    for(int i = 0; i<size;i++)
+        printf("%d\t",arreglo[i]);
+    printf("\n");
+}
+
+void IntMatrizPrinter(int arreglo[MaxPal][MaxDoc], int largo, int ancho){
+    for(int i = 0; i< largo; i++){
+        IntArrayPrinter(arreglo[i],ancho);
     }
 }
-    for(int i=0 ; i<50 ; i++) //este for es para recorrer todos los documentos
-    {
-        sprintf(nombre, "Doc%i.txt",i);
-        archivo=fopen(nombre,"r");
-        while(feof(archivo)==0) //mientras no llego al final del archivo
-        {
-            fscanf(archivo,"%s",buffer);
-            if(strlen(buffer)>3)
-            {
-                for(int k=0 ; k<1000 ; k++) //recorre el vocabulario
-                {
-                    if((strcmp(buffer,Vocabulario[k])==0) //si hay una palabra igual, la almacena en vocabulario
-                    {
-                        matriz[k][i]++; //aumenta en 1 el valor de la matriz llena de 0
+void CharArrayPrinter(char arreglo[MaxPal][LargoMaxPal], int size){
+    for(int i = 0; i<size;i++)
+        printf("%s\t",arreglo[i]);
+    printf("\n");
+}
+
+int ValorONegativo(int arreglo[], int posicion){
+    if(posicion<0){
+        return -2;
+    }else{
+        return arreglo[posicion];
+    }
+}
+
+void MayoresFrecuencias(string consulta,char Vocabulario[MaxPal][LargoMaxPal], int Matriz[MaxPal][MaxDoc], int Frecuencias[Listado]){
+    int Lugar = EncontrarLugar(consulta, Vocabulario);
+    if(Lugar != -1){
+        ExistePalabra = 1;
+        int a = 1;
+        int Freq[MaxDoc];
+        memset(Freq,0,Listado*sizeof(int));
+        memcpy(Freq,Matriz[Lugar],MaxDoc*sizeof(int));
+
+        int sol[3] = {-1,-1,-1};
+
+        for(int i = 0; i<MaxDoc; i++){
+            int carrier = i;
+            int temp = -1;
+            for(int j = 0; j<3; j++){
+                if(ValorONegativo(Freq,carrier)>ValorONegativo(Freq,sol[j])){
+                    if(j==2){
+                        sol[j] = carrier;
+                    } else {
+                        temp = sol[j];
+                        sol[j] = carrier;
+                        carrier = temp;
                     }
+                } else if(ValorONegativo(Freq,carrier)==ValorONegativo(Freq,sol[j]) && carrier<sol[j]) {
+                    temp = sol[j];
+                    sol[j] = carrier;
+                    carrier = temp;
                 }
             }
-            fclose(archivo);
+
         }
+        for(int k = 0; k<3; k++)
+            Frecuencias[k] = sol[k];
+    }else{
+        printf("\nNo se encontraron resultados para la palabra '%s'.\n",consulta);
     }
 
-void frecuencia(){
-    int frecuencia[3];
-    int aparicion[4]={2,0,3,1};
-    int masgrande=-1;
-    int segundogrande=-1;
-    int tercerogrande=-1;
 
-        for(i=0;i<4;i++){
-            if(aparicion[i]>masgrande){
-              tercerogrande=segundogrande;
-              frecuencia[2]=frecuencia[1];
-              segundogrande=masgrande;
-              frecuencia[1]=frecuencia[0];
-              masgrande=aparicion[i];
-              frecuencia[0]=i;
-        }else
-            if(aparicion[i]>segundogrande){
-              tercerogrande=segundogrande;
-              frecuencia[2]=frecuencia[1];
-              segundogrande=aparicion[i];
-              frecuencia[i]=i;
-        }else
-            if(aparicion[i]>tercerogrande){
-              frecuencia[2]=i;
-              tercerogrande=aparicion[i];
+}
+
+void MostrarResultadosDocumento(int NumeroDocumento){
+    printf("\nDoc%d.txt\n", NumeroDocumento);
+
+    int size = 0;
+    if (NumeroDocumento>9){
+        size = sizeof("Doc10.txt");
+    }else{
+        size = sizeof("Doc1.txt");
+    };
+    char filename[size];
+
+
+    sprintf(filename, "Doc%d.txt", NumeroDocumento);
+
+    char *contenido = NULL;
+    int fsize = 0;
+    FILE *fp;
+
+    fp = fopen(filename, "r");
+    if(fp) {
+        fseek(fp, 0, SEEK_END);
+        fsize = ftell(fp);
+        rewind(fp);
+
+        contenido = (char*) malloc(sizeof(char) * fsize);
+        fread(contenido, 1, fsize, fp);
+
+        fclose(fp);
+    }
+
+    const char delimitador[2] = " ";
+    char *token;
+
+    token = strtok(contenido, delimitador);
+
+    for(int k=0; k<4; k++){
+        printf( " %s\t", token );
+
+        token = strtok(NULL, delimitador);
+
+    }
+
+}
+
+void MostrarResultados(int Frecuencias[Listado]){
+    MostrarResultadosDocumento(Frecuencias[0]);
+    MostrarResultadosDocumento(Frecuencias[1]);
+    MostrarResultadosDocumento(Frecuencias[2]);
+
+}
+
+int ExisteArchivo(int NumeroDocumento){
+    int Existencia = 0;
+    int size = 0;
+    
+    if (NumeroDocumento>9){
+        size = sizeof("Doc10.txt");
+    }else{
+        size = sizeof("Doc1.txt");
+    };
+    char filename[size];
+
+
+    sprintf(filename, "Doc%d.txt", NumeroDocumento);
+
+    FILE *fp;
+
+    fp = fopen(filename, "r");
+    if(fp) {
+        Existencia = 1;
+        fclose(fp);
+    }
+    
+    return Existencia;
+}
+
+void EncontrarNumeroDeArchivos(){
+    for(int i = 49; i>=0; i--){
+        if (ExisteArchivo(i)==1){
+            NumeroArchivos = i +1;
+            break;
         }
+
     }
 }
-void imprimirPalabras(char palabras[20][20]){
-	for(int i = 0; i < 20; i++){
-		printf("%s",palabras[i]);
-	}
-}
+
+void CrearVocabulario(char Vocabulario[MaxPal][LargoMaxPal]){
+    memset(Vocabulario,0,MaxPal*LargoMaxPal*sizeof(char));
+    for(int i = 0; i< NumeroArchivos; i++){
+
+        int size = 0;
+        if (i>9){
+            size = sizeof("Doc10.txt");
+        }else{
+            size = sizeof("Doc1.txt");
+        };
+        char filename[size];
 
 
-int main(){
+        sprintf(filename, "Doc%d.txt", i);
 
-    for(int i=0;i<=50;i++){
-    char nombrearchivo[15];
-    sprintf(nombrearchivo,"Doc%i.txt",i);
-    FILE*archivo=fopen(nombrearchivo,"r");
-       if(archivo==NULL){
-       printf("el archivo %s no existe \n",nombrearchivo);
-       continue;
-       }
-       while(feof(archivo)==0){
-            fscanf(archivo,"%s",buffer);
-            if(strcmp(buffer,)==0){
-                int frec++;
+        char *contenido = NULL;
+        int fsize = 0;
+        FILE *fp;
+
+        fp = fopen(filename, "r");
+        if(fp) {
+            fseek(fp, 0, SEEK_END);
+            fsize = ftell(fp);
+            rewind(fp);
+
+            contenido = (char*) malloc(sizeof(char) * fsize);
+            fread(contenido, 1, fsize, fp);
+
+            fclose(fp);
+        }
+
+        const char delimitador[2] = " ";
+        char *token;
+
+        token = strtok(contenido, delimitador);
+
+        while(token != NULL){
+            int LargoToken = strlen(token);
+            if(LargoToken>LargoMinPal){
+                int Lugar = EncontrarLugar(token,Vocabulario);
+                if(Lugar == -1){
+                    Lugar = NumPal;
+                    NumPal++;
+                }
+                strcpy(Vocabulario[Lugar], token);
             }
-            Crear MatrizFreq();
+
+            token = strtok(NULL, delimitador);
         }
     }
+}
+
+void GenerarListaFrequencias(char NombreArchivo[], char Vocabulario[MaxPal][LargoMaxPal], int ListaFrec[MaxPal]){
+    memset(ListaFrec,0,MaxPal*sizeof(int));
+    char *contenido = NULL;
+    int fsize = 0;
+    FILE *fp;
+
+    fp = fopen(NombreArchivo, "r");
+    if(fp) {
+        fseek(fp, 0, SEEK_END);
+        fsize = ftell(fp);
+        rewind(fp);
+
+        contenido = (char*) malloc(sizeof(char) * fsize);
+        fread(contenido, 1, fsize, fp);
+
+        fclose(fp);
+    }
+
+    const char delimitador[2] = " ";
+    char *token;
+
+    token = strtok(contenido, delimitador);
+
+    while(token != NULL){
+        int LargoToken = strlen(token);
+        if(LargoToken>LargoMinPal){
+            int Lugar = EncontrarLugar(token,Vocabulario);
+
+            ListaFrec[Lugar] = ListaFrec[Lugar] + 1;
+        }
+
+        token = strtok(NULL, delimitador);
+    }
+
+
+}
+
+void CrearMatrizFrecuencias(char Vocabulario[MaxPal][LargoMaxPal], int Matriz[MaxPal][MaxDoc]){
+    memset(Matriz,0,MaxPal*LargoMaxPal*sizeof(int));
+
+    for(int i = 0; i< NumeroArchivos; i++){
+
+        int size = 0;
+        if (i>9){
+            size = sizeof("Doc10.txt");
+        }else{
+            size = sizeof("Doc1.txt");
+        };
+        char filename[size];
+
+
+        sprintf(filename, "Doc%d.txt", i);
+
+        int ListaFrec[MaxPal];
+        GenerarListaFrequencias(filename, Vocabulario, ListaFrec);
+        for(int j = 0; j<MaxPal; j++){
+            Matriz[j][i] = ListaFrec[j];
+        }
+    }
+}
+
+void GenerarMatrizDatos(char Vocabulario[MaxPal][LargoMaxPal],int Matriz[MaxPal][MaxDoc]){
+    EncontrarNumeroDeArchivos();
+    CrearVocabulario(Vocabulario);
+    CrearMatrizFrecuencias(Vocabulario,Matriz);
+}
+
+
+void main (){
+    char Vocabulario[MaxPal][LargoMaxPal];
+    int Matriz[MaxPal][MaxDoc];
+    string entrada;
+    int Frecuencias[3];
+
+    entrada = get_string("Ingrese Consulta:");
+
+
+    GenerarMatrizDatos(Vocabulario,Matriz);
+
+    MayoresFrecuencias(entrada,Vocabulario,Matriz,Frecuencias);
+
+
+
+    if(ExistePalabra)
+        MostrarResultados(Frecuencias);
+
+    printf("\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
